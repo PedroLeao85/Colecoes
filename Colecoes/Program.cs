d72f3f88a501630a30bbfc3197ddab88da31bcf6
PedroLeao85 <phalves1@hotmail.com>
2022-11-04 00:36:02 -0300

    Adiciona arrayInteiros
    Cria um array de inteiros e percorre o mesmo usando For e ForEach

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,4 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int [3];
+
+            arrayInteiros[0] = 15;
+            arrayInteiros[1] = 10;
+            arrayInteiros[2] = 20;
+
+            System.Console.WriteLine("Percorrendo o array pelo For");
+            for (int i = 0 ; i < arrayInteiros.Length ; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+            System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            foreach (int item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
 
