93f96546e3bc596bd9806272ac7c9321d94ec8de
PedroLeao85 <phalves1@hotmail.com>
2022-11-04 18:05:42 -0300

    Adiciona método Copiar
    Copiar o array antigo para um novo utilizando a classe Array.copy

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -9,15 +9,22 @@ namespace Colecoes
         {
             OperacoesArray op = new OperacoesArray();
             int[] array = new int[5] { 6, 3, 8, 1, 9 };
+            int[] arrayCopia = new int[10];
             
-            System.Console.WriteLine("Array original: ");
-            op.ImprimirArray(array);
+            //System.Console.WriteLine("Array original: ");
+            //op.ImprimirArray(array);
 
             //op.OrdenarBubbleSort(ref array);
-              op.Ordenar(ref array);  
+            //op.Ordenar(ref array);  
 
-            System.Console.WriteLine("Array Ordenado");
-            op.ImprimirArray(array);
+            //System.Console.WriteLine("Array Ordenado");
+            //op.ImprimirArray(array);
+
+            System.Console.WriteLine("Array antes da cópia");
+            op.ImprimirArray(arrayCopia);
+            op.Copiar(ref array, ref arrayCopia);
+            System.Console.WriteLine("Array após a cópia");
+            op.ImprimirArray(arrayCopia);
             
             // int [,] matriz = new int [4,2]
             // {
